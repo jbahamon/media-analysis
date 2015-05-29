5fc6828ff1e6326c96a21487b84a5cdf6e0e9f3b
Jorge Bahamonde <jbahamon@ug.uchile.cl>
2015-05-29 17:13:59 -0300

    wow such advancing

--- second_stage/correlate.py
+++ second_stage/correlate.py
@@ -4,21 +4,44 @@ import json
 import argparse as ap
 import os, sys
 from datetime import datetime
-from scipy.stats import spearmanr
+from scipy.stats import spearmanr, kurtosis
 date_format = "%Y-%m-%d"
 
-parser = ap.ArgumentParser(description = "Computes correlations for a " + \
+parser = ap.ArgumentParser(description = "Computes correlations for a " \
     "JSON file containing time series for a term.")
 
 parser.add_argument( "-i", "--in_file", type=ap.FileType("r"), default=sys.stdin, help = \
-        "The JSON files to read time series from. If not specified, standard " +\
+        "The JSON file to read time series from. If not specified, standard " \
         "input will be used.")
 
-parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the " + \
+parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the " \
         "output file.")
 
 parser.add_argument("-o", "--out_file", type=ap.FileType("w"), default=sys.stdout, help = \
     "The file to be output. If not specified, the standard output will be used.")
+
+
+def get_correlation_confidence(series1, series2):
+
+    if len(series1) != len(series2): 
+        raise ValueError("Series must have the same length")
+
+    series_length = len(series1)
+
+    correlations = []
+
+    for i in range(series_length):
+        s1_temp = series1[0:i] + series1[i+1:]
+        s2_temp = series2[0:i] + series2[i+1:]
+
+        correlations.push(spearmanr(s1_temp, s2_temp)[0])
+
+
+    mean_correlation = sum(correlations)/float(series_length)
+
+    k = kurtosis(correlations)
+
+    return (mean_correlation, k)
 
 
 args = parser.parse_args()
@@ -27,21 +50,32 @@ with args.in_file as json_file:
     with args.out_file as out_file:
         parsed_json = json.loads(json_file.read())
 
-        names = sorted(parsed_json["series"].keys())
+        terms = sorted(parsed_json["series"].keys())
+        names = sorted(parsed_json["series"][terms[0]].keys())
+
+        n_terms = len(terms)
         n_names = len(names)
+
         nodes = [ { \
                   "name": names[i], \
                   "index": i, \
                   "color_value": 0, \
                   "size": 20 } \
-                  for i in range(n_names) ] 
+                  for i in range(n_names) ]
+
+
         links = []
 
-        for i in range(n_names):
-            for j in range(i + 1, n_names):
-                spearman = spearmanr(parsed_json["series"][names[i]], \
+        for term, series in parsed_json["series"].items():
+            for i in range(n_names):
+                for j in range(i + 1, n_names):
+                    correlation_confidence = get_correlation_confidence(\
+                            parsed_json["series"][names[i]], \
                             parsed_json["series"][names[j]])
-                links.append( { "source": i, "target": j, "value": spearman })
+
+                    links.append( { "source": i, \
+                                    "target": j, \
+                                    "value" : correlation_confidence })
 
 
         if args.pretty:
--- second_stage/time_series.py
+++ second_stage/time_series.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 from unidecode import unidecode
 import string, codecs, re
 from datetime import datetime
+import math
 
 date_format = "%Y-%m-%d"
 
@@ -14,45 +15,53 @@ class readable_dir(ap.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         prospective_dir = values
         if not os.path.isdir(prospective_dir):
-            raise ap.ArgumentTypeError(("readable_dir:{0} is not a valid" + \
+            raise ap.ArgumentTypeError(("readable_dir:{0} is not a valid" \
                     "path").format(prospective_dir)) 
 
         if os.access(prospective_dir, os.R_OK):
             setattr(namespace, self.dest, prospective_dir)
         else:
-            raise ap.ArgumentTypeError(("readable_dir:{0} is not a" + \
+            raise ap.ArgumentTypeError(("readable_dir:{0} is not a" \
             "readable dir".format(prospective_dir)))
 
 # this is enough for now.
 normalize_text = lambda text: unidecode(text).strip().lower()
 
-parser = ap.ArgumentParser(description = "Computes time series for a" + \
+parser = ap.ArgumentParser(description = "Computes time series for a" \
     "folder of JSON files containing tweets and a term.")
 
 parser.add_argument("folder", action=readable_dir, help = \
         "The folder to read JSON files from.")
 
-parser.add_argument("term", help = "The terms to create the time series for."+ \
-        "These are treated as a single term")
+parser.add_argument("terms_file", type=ap.FileType("r"),  \
+        help = "A file with a single term per line. Time series " \
+        "will be created for each term.")
 
 parser.add_argument("-t", "--threshold", default=0, type=float, \
-        help = "The minimum average tweet-per-day to require. Default is no" +\
+        help = "The minimum average tweet-per-day to require. Default is no" \
         "threshold at all.")
 
-parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the " + \
+parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the "  \
         "output file.")
 
 parser.add_argument("-o", "--out_file", type=ap.FileType("w"), default=sys.stdout, help = \
     "The file to be output. If not specified, the standard output will be used.")
+
+
 #TODO: add:
 #   temporal granularity parameter
 #   time interval parameter
 #   method parameter
 
+patch_value = 0.000001
+
 args = parser.parse_args()
 
 folder = args.folder
-term = normalize_text(args.term)
+terms = []
+
+with args.terms_file as f:
+    terms = [ set(map(normalize_text, line.split())) for line in f.readlines() ]
 
 media_histogram = defaultdict(int)
 
@@ -62,9 +71,7 @@ files = [ filename for filename in os.listdir(folder) \
 
 files = sorted(files)
 
-# Date 
-
-mentions     = defaultdict(lambda: defaultdict(float))
+mentions     = { "+".join(map(str, t)) : defaultdict(lambda: defaultdict(float)) for t in terms }
 total_tweets = defaultdict(lambda: defaultdict(float))
 
 all_days = set()
@@ -79,10 +86,10 @@ for filename in files:
         
         for tweet in parsed_json:
             name = tweet["user"]["screen_name"]
-            text = normalize_text(tweet["text"])
-            
-            if args.term in text.split():
-                mentions[name][day] += 1
+            words = [ normalize_text(x) for x in tweet["text"].split() ]
+
+            for x in [ "+".join(map(str,term)) for term in terms if any(w in words for w in term) ]:
+                mentions[x][name][day] += 1
 
             total_tweets[name][day] += 1
             
@@ -93,25 +100,34 @@ day_range = datetime.strptime(all_days[-1], date_format) - \
 
 #FIXME: this might cause problems, as we're modifying things while reading them
 # better ask someone.
-for name, days in mentions.items():
+for term in terms:
+    term_mentions = mentions["+".join(map(str,term))]
+    for name, days in term_mentions.items():
 
-    # We remove the 
-    if sum( [ x for x in total_tweets[name].values() ] )/float(day_range.days) < args.threshold :
-        del mentions[name]
-        del total_tweets[name]
-        continue
+        # We remove the 
+        if sum( [ x for x in total_tweets[name].values() ] )/float(day_range.days) < args.threshold :
+            del term_mentions[name]
+            del total_tweets[name]
+            continue
 
-    # We force every day to appear.
-    for day in all_days:
-        days[day]
+        # We force every day to appear.
+        for day in all_days:
+            days[day]
 
-    for day, amount in days.items():
-        total_for_day = total_tweets[name][day]
-        mentions[name][day] = amount/total_for_day if total_for_day > 0 else 0
+        normalized_mentions = []
 
-    dayz = mentions[name]
-    mentions[name] = [ dayz[key] for key in sorted(dayz.keys()) ] 
+        for i in xrange(1,len(all_days)):
+            prev_day = days[all_days[i - 1]] + patch_value
+            day = days[all_days[i]] + patch_value
+            total_for_day = total_tweets[name][day] + patch_value
 
+            delta_amount = math.log( \
+                         (day      / (float(total_for_day) + patch_value)) / \
+                    float(prev_day / (float(total_for_day) + patch_value) ))
+
+            normalized_mentions.append(delta_amount)
+
+        term_mentions[name] = normalized_mentions
 
 with args.out_file as out_file:
 
@@ -124,6 +140,3 @@ with args.out_file as out_file:
         out_file.write(json.dumps( \
             { "time_labels" : all_days, \
               "series": mentions }))
-              
-
-        
