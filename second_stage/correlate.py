fixed inccorrect behavior where points were removed after normalization, not before

second_stage/correlate.py
@@ -7,27 +7,43 @@ from datetime import datetime
 import numpy as np
 from scipy import stats
 from scipy.stats import spearmanr, kurtosis
-from math import sqrt
+from math import sqrt, log
 
 import warnings
 warnings.filterwarnings('error')
 date_format = "%Y-%m-%d"
 
-parser = ap.ArgumentParser(description = "Computes correlations for a " \
-    "JSON file containing time series for a term.")
+def positive_float(x):
+    x = float(x)
+    if x <= 0:
+        raise argparse.ArgumentTypeError("Must be a positive float.")
+    return x
 
-parser.add_argument( "-i", "--in_file", type=ap.FileType("r"), default=sys.stdin, help = \
-        "The JSON file to read time series from. If not specified, standard " \
-        "input will be used.")
+def normalize_series(time_series, args):
+         
+    try:
+        prev_value = float(time_series[0]) 
+        # This deletes it from time_series.
+        time_series = time_series[1:]
+    except KeyError:
+        return None
+  
+    for k, current_value in enumerate(time_series):
 
-parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the " \
-        "output file.")
+        if args.ext:
+            if current_value != last_value:
+                time_series[k] = current_value - last_value
+                last_value = current_value
+            else:
+                time_series[k] = 0
+        else:
+            time_series[k] = log(current_value + args.log) -\
+                log(prev_value + args.log)
+            prev_value = current_value
 
-parser.add_argument("-o", "--out_file", type=ap.FileType("w"), default=sys.stdout, help = \
-    "The file to be output. If not specified, the standard output will be used.")
+    return time_series 
 
-
-def get_correlation_confidence(series1, series2):
+def get_correlation_confidence(series1, series2, args):
 
     if len(series1) != len(series2): 
         raise ValueError("Series must have the same length")
@@ -44,9 +60,11 @@ def get_correlation_confidence(series1, series2):
         s1_temp = series1[0:i] + series1[i+1:]
         s2_temp = series2[0:i] + series2[i+1:]
         
+        s1_norm = normalize_series(s1_temp, args)
+        s2_norm = normalize_series(s2_temp, args)
+        
         if sum(s1_temp) > 0 and sum(s2_temp) > 0:
-            spear = spearmanr(s1_temp, s2_temp)
-            correlations.append(spearmanr(s1_temp, s2_temp)[0])
+            correlations.append(spearmanr(s1_norm, s2_norm)[0])
 
     if len(correlations) == 0:
         return (0, 1, 0)
@@ -62,9 +80,32 @@ def get_correlation_confidence(series1, series2):
     else:
         CI_size = 0
         k = 0
-    #TODO: return full correlation instead of mean.
-    return (mean, CI_size, n)
+    
+    full_correlation = spearmanr(normalize_series(series1, args),\
+            normalize_series(series2, args))[0]
+    return (full_correlation, CI_size, n)
 
+
+parser = ap.ArgumentParser(description = "Computes correlations for a " \
+    "JSON file containing time series for a term.")
+
+parser.add_argument( "-i", "--in_file", type=ap.FileType("r"), default=sys.stdin, help = \
+        "The JSON file to read time series from. If not specified, standard " \
+        "input will be used.")
+
+parser.add_argument("-p", "--pretty", action="store_true", help = "Pretty print the " \
+        "output file.")
+
+parser.add_argument("-o", "--out_file", type=ap.FileType("w"), default=sys.stdout, help = \
+    "The file to be output. If not specified, the standard output will be used.")
+
+normalizer = parser.add_mutually_exclusive_group(required=True)
+normalizer.add_argument("-l", "--log", nargs="?", const=1, type=positive_float,\
+        help = "Use this value as a additive "\
+        "base to normalize using difference of logarithms.")
+
+normalizer.add_argument("-e", "--ext", action="store_true", help = "Extend "\
+                        "nonzero values and normalize using linear differences.")
 
 args = parser.parse_args()
 
@@ -94,7 +135,8 @@ with args.in_file as json_file:
 
                     correlation_confidence = get_correlation_confidence(\
                             series[names[i]][0], \
-                            series[names[j]][0])
+                            series[names[j]][0],
+                            args)
 
                     links[term].append( { "source": i, \
                                     "target": j, \