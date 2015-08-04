f04d7c52a3e9915a29764425e9752502b6630577
Jorge Bahamonde <jbahamon@ug.uchile.cl>
2015-08-04 17:34:44 -0300

    Enchulando visualizaciones:)

--- first_stage/tf-idf.py
+++ first_stage/tf-idf.py
@@ -12,10 +12,6 @@ def tokenize(text):
 vectorizer = TfidfVectorizer(tokenizer=tokenize,
         stop_words=nltk.corpus.stopwords.words('spanish'))
 
-def cosine_sim(text1, text2):
-    tfidf = vectorizer.fit_transform([text1, text2])
-    return ((tfidf * tfidf.T).A)[0,1]
-
 with open(sys.argv[1], "r") as source_file:
     with open(sys.argv[2], "w") as destination_file:
         news_data = source_file.readlines()
